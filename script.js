d35f2ba6c5d0eb23c3ae39b96434c8bffee786e1
helengorban <114924748+helengorban@users.noreply.github.com>
2023-03-19 15:45:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,42 +6,41 @@ const minussign = document.querySelector(".minus-btn");
 const multiplysign = document.querySelector(".multiply-btn");
 const dividesign = document.querySelector(".divide-btn");
 
-const displayResult = document.querySelector("#displayresult")
+const displayResult = document.querySelector("#displayresult");
 
 plusSign.addEventListener("click", addition);
-minussign.addEventListener("click", substraction)
-multiplysign.addEventListener("click", multiplication)
-dividesign.addEventListener("click", division)
+minussign.addEventListener("click", substraction);
+multiplysign.addEventListener("click", multiplication);
+dividesign.addEventListener("click", division);
 
-
-function addition (){
-    let number1 = field1.valueAsNumber;
-    let number2 = field2.valueAsNumber;
-    displayResult.textContent = number1 + number2;
-    // return result
-    return result
+function addition() {
+  let number1 = field1.valueAsNumber;
+  let number2 = field2.valueAsNumber;
+  displayResult.textContent = number1 + number2;
+  // return result
+  return result;
 }
 
-function substraction (){
-    let number1 = field1.valueAsNumber;
-    let number2 = field2.valueAsNumber;
-    displayResult.textContent = number1 - number2;
-    // return result
-    return result
+function substraction() {
+  let number1 = field1.valueAsNumber;
+  let number2 = field2.valueAsNumber;
+  displayResult.textContent = number1 - number2;
+  // return result
+  return result;
 }
 
-function multiplication (){
-    let number1 = field1.valueAsNumber;
-    let number2 = field2.valueAsNumber;
-    displayResult.textContent = number1 * number2;
-    // return result
-    return result
+function multiplication() {
+  let number1 = field1.valueAsNumber;
+  let number2 = field2.valueAsNumber;
+  displayResult.textContent = number1 * number2;
+  // return result
+  return result;
 }
 
-function division (){
-    let number1 = field1.valueAsNumber;
-    let number2 = field2.valueAsNumber;
-    displayResult.textContent = number1 / number2;
-    // return result
-    return result
+function division() {
+  let number1 = field1.valueAsNumber;
+  let number2 = field2.valueAsNumber;
+  displayResult.textContent = number1 / number2;
+  // return result
+  return result;
 }
